79cd2d5bce1f17fbde3acd5062c5afcb134a670e
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2015-05-05 08:13:08 +0200

    FutureSupport.sequentialExecution is same as Future.sequence

--- src/test/scala/io/vamp/common/akka/FutureSupportSpec.scala
+++ src/test/scala/io/vamp/common/akka/FutureSupportSpec.scala
@@ -11,8 +11,8 @@ import scala.concurrent.Future
 
 class FutureSupportSpec extends FunSuite with Matchers with Futures with ScalaFutures {
 
+  object support extends FutureSupport
   test("FutureSuppport.sequentialExecution does not do Sequential execution as the name implies.") {
-    object support extends FutureSupport
     val orderOfExecution = Collections.synchronizedList(new util.ArrayList[String]())
 
     val future2 = Future {
@@ -28,10 +28,22 @@ class FutureSupportSpec extends FunSuite with Matchers with Futures with ScalaFu
 
     val result = support.sequentialExecution(List(future1, future2))
 
-    println(result)
     result.futureValue should be(List("Future1", "Future2"))
     orderOfExecution.get(0) should be("Future1")
     orderOfExecution.get(1) should be("Future2")
+  }
+
+  test("FutureSupport.sequentialExecution is part of the library") {
+    val future2 = Future {
+      "Future2"
+    }
+    val future1 = Future {
+      // Needed to force that this future will complete somewhat later
+      Thread.sleep(10)
+      "Future1"
+    }
+    val result = support.sequentialExecution(List(future1, future2))
+    result.futureValue should be (Future.sequence(List(future1, future2)).futureValue)
   }
 
 }
